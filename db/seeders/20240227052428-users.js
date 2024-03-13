1cb981d32323eef4f3b7b514a32c19023acf496d
adithyan <adithyanpradeep@36>
2024-03-13 11:26:42 +0530

    react

--- db/seeders/20240227052428-users.js
+++ db/seeders/20240227052428-users.js
@@ -1,42 +1,40 @@
-
 'use strict';
 
 module.exports = {
   up: (models, mongoose) => {
     return models.users
-    .insertMany ([{
-      _id : "65e557b0e06e940dfa023fa2",
-
-      
-        first_name : "mark",
-        last_name : "antony",
-        email : 'mark@gmail.com',
-        password : "$2a$12$0gY8ytInp36Mef90KKKnpOWNhT789TunW/bMwayE2fSUvLod3/7v6",
-
-    }
-  ])
-  .then((res)=>{
-    console.log(res.insertMany)
-  })
-
-
-    
+    .insertMany([
+      {
+        _id:  "65e15c64ffd472dd229cc437",
+        firstname: "adhi",
+        lastname: "cr",
+        email: "adhi@gmail.com",
+        password: "$2a$12$9fhgl.9Kr9KLWoK5thvxk.qbACUNZO319PQddk4P3NIFEfhOWHMJO",
+        phone: "9048475227"
+      },
+      {
+        _id: "65e15c92ffd472dd229cc438",
+        firstname: "abin",
+        lastname : "sasi",
+        email: "abin@gmail.com",
+        password: "$2a$12$nIlueSiZI3D0DS54SjtUuOm7hQvj9FR91I5OW4K.AaAOtBRSgrJsm",
+        phone: "9999958555"
+      }
+    ])
   },
 
   down: (models, mongoose) => {
     return models.users
     .deleteMany({
       _id : {
-        $in : [
-          "65e557b0e06e940dfa023fa2",
-         
-
-        ]
-      }
+        $in: [
+          "65e15c64ffd472dd229cc437",
+          "65e15c92ffd472dd229cc438"
+        ],
+      },
     })
     .then((res)=>{
-      console.log(res.deletedCount)
-    });
- 
-  },
+      console.log(res.deletedCount);
+    })
+  }
 };
